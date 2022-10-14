e129b577f9a4aafb1a8eaeffc98aabaa51b65a81
Bhaskar Dhara <dharabhaskar@gmail.com>
2022-10-14 20:58:34 +0530

    initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import {saveAs} from 'file-saver'
 
 function App() {
+  const [url, setUrl] = useState([])
+
+  const download = (e) => {
+    e.preventDefault()
+    saveAs(url, 'image.png')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={download}>
+        <input placeholder='url' onChange={(e) => setUrl(e.target.value)}/>
+        <button type='submit'>download</button>
+      </form>
     </div>
   );
 }
