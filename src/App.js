564c7ed49710ef266d504c0fae79f8a522a58692
Bhaskar Dhara <dharabhaskar@gmail.com>
2022-10-14 21:02:13 +0530

    nameSetting

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import {saveAs} from 'file-saver'
 
 function App() {
   const [url, setUrl] = useState([])
+  const [name, setName] = useState([])
 
   const download = (e) => {
     e.preventDefault()
-    saveAs(url, 'image.png')
+    saveAs(url, `${name}.png`)
   }
 
   return (
     <div className="App">
       <form onSubmit={download}>
         <input placeholder='url' onChange={(e) => setUrl(e.target.value)}/>
+        <input placeholder='name' onChange={(e) => setName(e.target.value)}/>
         <button type='submit'>download</button>
       </form>
     </div>
